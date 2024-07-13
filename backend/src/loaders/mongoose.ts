c5cc28e54cd49a6d76267aa97f9daa8289cd3c3e
Leon (Weidong Li) <leonlidawn@gmail.com>
2024-07-13 16:19:49 +1000

    draft

--- backend/src/loaders/mongoose.ts
+++ backend/src/loaders/mongoose.ts
@@ -4,10 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const url = process.env.DB_CONNECTION?? "";
-<<<<<<< HEAD
-=======
-
->>>>>>> c087ff499e6c90edc0a297b01dd76a0299f8d877
 export async function initDB() {
     mongoose.set('strictQuery', false);
     await mongoose.connect(url);
---
+++ notes.md
@@ -0,0 +1,7 @@
+- use nvmrc to specify node versions
+- resolve conflicts
+
+- @babel/plugin-proposal-private-property-in-object
+- why?
+
+- eslint plugin flowtype required?
